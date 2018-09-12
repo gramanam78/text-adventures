25f97debb8611d05128a4a52576d3143a75e3fc4
Merit Student <student@meac.local>
2018-09-12 13:31:11 -0600

    my hand slipped and i made a game

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("working")
+print("to start, type \'YEET\'")
+fromuser = input()
+while True:
+    if fromuser== "YEET":
+        print("Start Game!")
+       elif fromuser == "quit
+        print("please don't leave, i am very lonely")
+        break
+    else:
+        print("thou canst followest instruction ye nincompoop") 
+    fromuser = input()
+
+
+
+
+
+
+
+
